a6ff68d0fcf27eaa4a964c22be7d470594c2bbec
Takeru-chan <takeru.katayama@gmail.com>
2017-07-17 08:13:03 +0900

    Fix to get data.

--- stock.sh
+++ stock.sh
@@ -4,10 +4,10 @@ TARGET='./stock.list'
 LOG='./price.list'
 for CODE in `head -1 $TARGET | tr ',' ' '`
 do
-  RESULT=`curl -s "https://www.google.com/finance/getprices?p=1Y&i=86400&x=TYO&q=${CODE}" |\
+  RESULT=`curl -s "https://www.google.com/finance/getprices?p=1d&i=86400&x=TYO&q=${CODE}" |\
     grep -e '^a[0-9,.]*$' | cut -d ',' -f 1,2`
   PRICE=${PRICE}","`echo $RESULT | cut -d ',' -f 2`
 done
-DATE=$((`echo $RESULT | cut -d ',' -f 1 | tr -d 'a'` - 86400))
+DATE=`echo $RESULT | cut -d ',' -f 1 | tr -d 'a'`
 date -jf "%s" $DATE "+%Y年%m月%d日" > $LOG
 echo $PRICE | sed 's/^,//' >> $LOG
